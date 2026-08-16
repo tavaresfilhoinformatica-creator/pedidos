15128ae37b45f31cb9ea1e1dee521e20fb42b140
acozinha0629 <tavaresfilhoinformatica@gmail.com>
2026-08-16 12:52:45 -0300

    verificar  SMS ao receber novo pedido

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+console.log("Telefone carregado:", process.env.SEU_TELEFONE);
+console.log("Usuario ClickSend:", process.env.CLICKSEND_USERNAME);
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
